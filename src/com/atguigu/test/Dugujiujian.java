b8bf2f6848eca04d8d23699d86aadc20e05d924c
linghuchong88 <linghuchong88@126.com>
2016-11-23 08:33:01 +0800

    update 04

--- src/com/atguigu/test/Dugujiujian.java
+++ src/com/atguigu/test/Dugujiujian.java
@@ -5,8 +5,10 @@ public class Dugujiujian {
 	public static void main(String[] args) {
 		
 		System.out.println("hello dugu");
-
                 System.out.println("hello dugu22222222222222");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
 	}
 
 }
