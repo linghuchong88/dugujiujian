83e5557b2fff4906f4d979f9b87863bf9c460220
令狐冲 <linghuchong88@126.com>
2016-11-24 14:54:38 +0800

    New Func

--- src/com/atguigu/test/Dugujiujian.java
+++ src/com/atguigu/test/Dugujiujian.java
@@ -5,12 +5,15 @@ public class Dugujiujian {
 	public static void main(String[] args) {
 		
 		System.out.println("hello dugu");
+        System.out.println("hello dugu22222222211122222");
         System.out.println("hello dugu22222222222222");
         System.out.println("hello dugu22222222222222");
         System.out.println("hello dugu22222222222222");
         System.out.println("hello dugu22222222222222");
         System.out.println("hello dugu22222222222222");
-                System.out.println("hello dugu22222222222222");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
 		System.out.println("hello");
 		System.out.println("hello");
 		System.out.println("hello");
